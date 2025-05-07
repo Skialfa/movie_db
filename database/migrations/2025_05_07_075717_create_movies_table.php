25c0c40b803df35b440ba5c55ac68a9e964b4ef9
Dimas Aditya Ramadhan <dimas.adityaramadhan071104@gmail.com>
2025-05-07 15:18:26 +0700

    second commit

---
+++ database/migrations/2025_05_07_075717_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id(); // bigint unsigned auto_increment
+            $table->string('title', 255);
+            $table->string('slug', 255);
+            $table->text('synopsis');
+            $table->unsignedBigInteger('category_id');
+            $table->year('year');
+            $table->text('actors')->nullable();
+            $table->string('cover_image', 255)->nullable();
+            $table->timestamps(); // created_at & updated_at
+
+            // Relasi foreign key ke categories
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};

2dc41abf55c95235ab404b6a262094b1429eb9f4
Dimas Aditya Ramadhan <dimas.adityaramadhan071104@gmail.com>
2025-05-07 16:04:30 +0700

    third commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Movie;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            CategorySeeder::class,
         ]);
+
+        Movie::factory(50)->create();
+
+        //User::factory()->create([
+          //  'name' => 'Test User',
+            //'email' => 'test@example.com',
+        //]);
     }
 }
